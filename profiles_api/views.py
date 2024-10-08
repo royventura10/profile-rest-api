49d5582b52692db0f06d2e0ab131f80640beab6f
Roy Ventura <roy21vp@gmail.com>
2024-10-08 15:33:17 -0500

    Enable DJANGO admin for user profile model

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+
+    def get(self,request, format=None):
+        """Tenemos que crear una diccionario o lista para enviarlo"""
+        an_apiview=[
+            '1. Hola 1',
+            '2. Hola 2',
+            '3. Hola 3',
+        ]
+        return Response({'message':'Hello!','an_apiview':an_apiview})
+
+
