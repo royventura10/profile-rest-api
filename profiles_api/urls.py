cc2959058ca4e28e87df43fda2e766dce0c0af65
Roy Ventura <roy21vp@gmail.com>
2024-10-12 02:06:40 -0500

    Added viewSet and viewAPI

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,6 +1,16 @@
-from django.urls import path
+from django.urls import path,include
 from profiles_api import views
 
+
+# Para los view set necesito utilizar un enrutador.
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+router.register('hello-viewset',views.HelloViewSet,base_name='hello-viewset')
+
+
+# Le agrego include para que envie las rutas a la lista de URL
 urlpatterns=[
-    path('hello-view/',views.HelloApiView.as_view())
+    path('hello-view/',views.HelloApiView.as_view()),
+    path('',include(router.urls))
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,17 @@
+from crypt import methods
+
+from pyexpat.errors import messages
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
 
+from profiles_api import serializers
 
 class HelloApiView(APIView):
+
+
+    serializer_class=serializers.HelloSerializer
 
     def get(self,request, format=None):
         """Tenemos que crear una diccionario o lista para enviarlo"""
@@ -12,5 +21,73 @@ class HelloApiView(APIView):
             '3. Hola 3',
         ]
         return Response({'message':'Hello!','an_apiview':an_apiview})
+
+    def post(self,request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name=serializer.validated_data.get('name')
+            #email=serializer.validated_data.get('email')
+            message = f'Hello {name}'
+            return Response({'menssage': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self,request,pk=None):
+        return Response({"method":'PUT'})
+
+    def patch(self,resquest,pk=None):
+        return Response({'method':'PATCH'})
+
+    def delete(self,request,pk=None):
+        return Response({'method' 'DELETE'})
+
+
+
+class HelloViewSet(viewsets.ViewSet):
+
+    # Utilizamos el mismo Serializer, tmb lo llamamos
+    serializer_class=serializers.HelloSerializer
+
+    def list(self,request):
+        a_viewset=[
+            'User01',
+            'User02',
+            'User03',
+        ]
+        return Response({'message':'Hello','a_viewset':a_viewset})
+
+    def create(self,request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            # email=serializer.validated_data.get('email')
+            message = f'Hello {name}'
+            return Response({'menssage': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self,request,pk=None):
+        return Response({'http_method':'GET'})
+
+    def update(self,request,pk=None):
+        return Response({'http_method':'PUT'})
+
+    def partial_update(self,request,pk=None):
+        return Response({'http_method':'PATCH'})
+
+    def destroy(self,request,pk=None):
+        return Response({'http_method':'DELETE'})
+
+
+
+
 
 
